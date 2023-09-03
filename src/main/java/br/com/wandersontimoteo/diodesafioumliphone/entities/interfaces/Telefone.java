cb801d4a8f984509a17c8bb9335be2a9347af15c
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-09-03 16:09:41 -0400

    feat: added interface Telefone

---
+++ src/main/java/br/com/wandersontimoteo/diodesafioumliphone/entities/interfaces/Telefone.java
@@ -0,0 +1,8 @@
+package br.com.wandersontimoteo.diodesafioumliphone.entities.interfaces;
+
+public interface Telefone {
+    public void fazerLigacao();
+    public void atenderLigacao();
+    public void correioVoz();
+    public void excluirMensagemCorreioVoz();
+}
