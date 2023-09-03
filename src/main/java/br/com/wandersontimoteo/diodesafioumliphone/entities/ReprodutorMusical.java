7c77a433bce087fe3c67c20cb0e4f59f40f8e70d
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-09-03 15:55:45 -0400

    feat: add interface Reprodutor Musical

---
+++ src/main/java/br/com/wandersontimoteo/diodesafioumliphone/entities/ReprodutorMusical.java
@@ -0,0 +1,10 @@
+package br.com.wandersontimoteo.diodesafioumliphone.entities;
+
+public interface ReprodutorMusical {
+    public void iniciarMusica();
+    public void pararMusica();
+    public void selecionarMusica();
+    public void criarListaMusica();
+    public void excluirListaMusica();
+}
+
