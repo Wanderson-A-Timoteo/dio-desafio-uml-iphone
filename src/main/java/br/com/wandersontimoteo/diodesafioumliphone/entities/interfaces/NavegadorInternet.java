566f8879fb6da9d6ba1612447bd8bfd36232b023
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-09-03 16:12:37 -0400

    feat: added interface Navegador de Internet

---
+++ src/main/java/br/com/wandersontimoteo/diodesafioumliphone/entities/interfaces/NavegadorInternet.java
@@ -0,0 +1,8 @@
+package br.com.wandersontimoteo.diodesafioumliphone.entities.interfaces;
+
+public interface NavegadorInternet {
+    public void exibirPagina();
+    public void atualizarPagina();
+    public void adicionarNovaAba();
+    public void excluirPagina();
+}
