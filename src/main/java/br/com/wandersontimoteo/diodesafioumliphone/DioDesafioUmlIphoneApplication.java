8adfcccaecb256b81fb3956812bf064f8fd4efee
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-09-03 12:21:00 -0400

    feat: Created project

---
+++ src/main/java/br/com/wandersontimoteo/diodesafioumliphone/DioDesafioUmlIphoneApplication.java
@@ -0,0 +1,13 @@
+package br.com.wandersontimoteo.diodesafioumliphone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioDesafioUmlIphoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioDesafioUmlIphoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/wandersontimoteo/diodesafioumliphone/DioDesafioUmlIphoneApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wandersontimoteo.diodesafioumliphone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioDesafioUmlIphoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
